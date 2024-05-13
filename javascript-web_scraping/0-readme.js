e117abb27a72937909437535ee964c9efe960d2b
Mottais <mottais.afy@outlook.fr>
2024-05-13 14:07:50 +0200

    Modif nom  des variables

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
-/* imprime dans la console le contenu d'un fichier ou err si erreur */
+/* imprime dans la console le contenu d'un fichier ou erreur si fichier n'a pas pu être lu */
 
 const fs = require('fs');
 const cheminDuFichier = process.argv[2];
 fs.readFile(cheminDuFichier, 'utf8', callbackLectureFichier);
 
 // Définition de la fonction de rappel
-function callbackLectureFichier (err, data) {
-  if (err) {
-    console.log(err);
+function callbackLectureFichier (erreur, contenu) {
+  if (erreur) {
+    console.log(erreur);
   } else {
-    console.log(data);
+    console.log(contenu)
   }
 }
