2119fbf124295e9c52061e0c178baa4ae6822a8e
Mottais <mottais.afy@outlook.fr>
2024-05-14 11:53:35 +0200

    Task

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -7,13 +7,14 @@ const url = process.argv[2];
 request(url, { json: true }, (erreur, response) => {
   if (erreur) {
     console.error(erreur);
-    return;
-  }
-  const completedByUser = {};
-  for (const task in response.body) {
-    if (response.body[task].completed === true) {
-      completedByUser[response.body[task].userId] = (completedByUser[response.body[task].userId] || 0) + 1;
+  } else {
+    const completedByUser = {};
+    for (const task in response.body) {
+      if (response.body[task].completed === true) {
+        completedByUser[response.body[task].userId] = (completedByUser[response.body[task].userId] || 0) + 1;
+      }
     }
+    console.log(completedByUser);
   }
-  console.log(completedByUser);
+
 });
