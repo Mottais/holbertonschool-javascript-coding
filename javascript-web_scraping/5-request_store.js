f8311aa44628c1f01a0c0d70dae799ebf2af616d
Mottais <mottais.afy@outlook.fr>
2024-05-14 10:26:24 +0200

    Task5

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/* Script qui récupère le contenu d'une page web et le stocke dans un fichier. */
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const path = process.argv[3];
+
+request.get(url, (erreur, reponse) => {
+  if (erreur) {
+    console.error(erreur);
+  } else {
+    fs.writeFile(path, reponse.body, 'utf-8', (err) => {
+      if (err) {
+        console.error('Error writing to file:', err);
+      } else {
+        console.log('File written successfully!');
+      }
+    });
+  }
+});
