e69bd3fc03cca83e9f2288669f7ba1196be7b34b
Mottais <mottais.afy@outlook.fr>
2024-05-22 13:07:24 +0200

    Task1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+function useStdin() {
+  process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  });
+}
+module.exports = useStdin;
+if (require.main === module) {
+  useStdin();
+}
