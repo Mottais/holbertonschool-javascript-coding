88a7dbc42220f0fb505f6080f9616a29c57798b9
Mottais <mottais.afy@outlook.fr>
2024-05-23 17:52:55 +0200

    test

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -32,15 +32,17 @@ function countStudents(path) {
 
       // Affichage du nombre total d'étudiants
       let output = `Number of students: ${totalStudents}`;
-      console.log(output);
+      // console.log(output);
 
       // Affichage des informations pour chaque clé dans l'objet students
       Object.keys(students).forEach((key) => {
         const count = students[key].length;
         const list = students[key].join(', ');
         output += `\nNumber of students in ${key}: ${count}. List: ${list}`;
-        console.log(`Number of students in ${key}: ${count}. List: ${list}`);
+      // console.log(`Number of students in ${key}: ${count}. List: ${list}`);
       });
+
+      console.log(output);
     })
     .catch(() => {
       throw new Error('Cannot load the database');
