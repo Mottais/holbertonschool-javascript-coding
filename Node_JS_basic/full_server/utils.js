7904da4196d16898d761b74bbbf8401f09df03a8
Mottais <mottais.afy@outlook.fr>
2024-05-24 15:53:54 +0200

    test

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,32 +1,37 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 
-function readDatabase(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (error, data) => {
-      if (error) { reject(new Error('Cannot load the database')); return; }
+// fonction pour créer un objet à partir des données
+// clé = field et valeur = tableau de prénoms
+function parseDataToObject(data) {
+  const lines = data.split('\n');
+  const result = {};
 
-      const lines = data.trim().split('\n').filter((line) => line.trim() !== '');
-      const header = lines[0].split(',');
+  // i= 1 pour ignorer la première ligne d'en-tête
+  for (let i = 1; i < (lines.length - 1); i += 1) {
+    // Je suppose que les champs existent et sont toujours dans le même ordre
+    const [firstname, , , field] = lines[i].split(',');
 
-      const students = lines.slice(1).map((line) => {
-        const values = line.split(',');
-        return {
-          firstname: values[header.indexOf('firstname')],
-          field: values[header.indexOf('field')],
-        };
-      });
+    if (field) {
+      // si la clé n'existe pas, je la crée
+      if (!result[field]) { result[field] = []; }
+      result[field].push(firstname);
+    }
+  }
 
-      const fieldCounts = {};
-      students.forEach((student) => {
-        if (fieldCounts[student.field] === undefined) {
-          fieldCounts[student.field] = [];
-        }
-        fieldCounts[student.field].push(student.firstname);
-      });
-
-      resolve(fieldCounts);
-    });
-  });
+  return result;
 }
 
-module.exports = readDatabase;
+function countStudents(path) {
+  return fs.readFile(path, 'utf8')
+    .then((data) => {
+      const students = parseDataToObject(data);
+
+      // Retourner le résultat pour un usage ultérieur
+      return students;
+    })
+    .catch(() => {
+      throw new Error('Cannot load the database');
+    });
+}
+
+module.exports = countStudents;
