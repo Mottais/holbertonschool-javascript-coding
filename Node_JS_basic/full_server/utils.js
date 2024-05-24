aae30eeefff07f9b3f3ce2ee9c5050084fa65992
Mottais <mottais.afy@outlook.fr>
2024-05-24 16:00:17 +0200

    Etat final Task8

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,32 +1,21 @@
 const fs = require('fs').promises;
 
-// fonction pour créer un objet à partir des données
-// clé = field et valeur = tableau de prénoms
 function parseDataToObject(data) {
   const lines = data.split('\n');
   const result = {};
-
-  // i= 1 pour ignorer la première ligne d'en-tête
   for (let i = 1; i < (lines.length - 1); i += 1) {
-    // Je suppose que les champs existent et sont toujours dans le même ordre
     const [firstname, , , field] = lines[i].split(',');
-
     if (field) {
-      // si la clé n'existe pas, je la crée
       if (!result[field]) { result[field] = []; }
       result[field].push(firstname);
     }
   }
-
   return result;
 }
-
 function countStudents(path) {
   return fs.readFile(path, 'utf8')
     .then((data) => parseDataToObject(data))
-    .catch(() => {
-      throw new Error('Cannot load the database');
-    });
+    .catch(() => { throw new Error('Cannot load the database'); });
 }
 
 module.exports = countStudents;
