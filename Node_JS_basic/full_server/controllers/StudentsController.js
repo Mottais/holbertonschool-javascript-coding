d4cc6706decbdffc25b6ad0304b7a0115c953fb6
Mottais <mottais.afy@outlook.fr>
2024-05-24 11:29:18 +0200

    test

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -6,6 +6,7 @@ class StudentsController {
       .then((result) => {
         let responseStr = 'This is the list of our students';
         Object.entries(result).forEach(([key, value]) => {
+          console.log(key, value);
           responseStr += `\nNumber of students in ${key}: ${value.length}. List: ${value.join(', ')}`;
         });
         response.status(200);
